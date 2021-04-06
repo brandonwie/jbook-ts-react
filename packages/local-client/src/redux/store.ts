a268f9f47dec4e384a1ecc002c400d14f0e3b9c5
Neo Wie <it.brandonwie@gmail.com>
2021-04-06 20:27:11 +0800

    test: created a dispatch for test

--- packages/local-client/src/redux/store.ts
+++ packages/local-client/src/redux/store.ts
@@ -3,9 +3,18 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import { persisMiddleware } from './middlewares/persist-middleware';
+import { CellActionType } from './action-types';
 
 export const store = createStore(
 	reducers,
 	{},
 	composeWithDevTools(applyMiddleware(thunk, persisMiddleware))
 );
+
+store.dispatch({
+	type: CellActionType.INSERT_CELL_AFTER,
+	payload: {
+		id: null,
+		type: 'text',
+	},
+});
